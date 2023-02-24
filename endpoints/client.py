cff69efeffd79d0b4f08a0fcbda8043828b0f6f8
rk <rebeccakruk@outlook.com>
2023-02-24 14:57:14 -0700

    client login and update complete

--- endpoints/client.py
+++ endpoints/client.py
@@ -1,52 +1,9 @@
 from app import app, __init__
 from flask import jsonify, make_response, request
 from dbhelpers import run_statement
-import uuid
 import bcrypt
-
-@app.get('/api/client')
-def get_client(token:str):
-    # token = session.get("token")
-    result = run_statement("CALL get_client(?)", [token])
-    keys = ["clientId", "createdAt", "email", "firstName", "lastName", "username"]
-    response = []
-    if (type(result) == list):
-
-        for client in result:
-            print(response)
-            response.append(dict(zip(keys, client)))
-        return make_response(jsonify(response), 200)
-    else:
-        print(result)
-        return make_response(jsonify(result), 500)
+import uuid
     
-@app.post('/api/client-login')
-def login_client():
-    email = request.json.get("email")
-    client_id = run_statement("CALL get_id(?)", [email])
-    password = request.json.get("password")
-    salt = bcrypt.gensalt()
-    hash_result = bcrypt.hashpw(password.encode(), salt)
-
-    
-    
-
-    #get procedure that grabs the hashed password and the id if it exists
-    #then do pw verification if, else
-    
-    token = uuid.uuid4().hex
-    keys = ["clientId", "token"]
-    results = run_statement("CALL client_login(?, ?)", [client_id, token])
-    if (type(results) == list):
-    # check the row count here; 0 is not successful
-            response = {
-                        "clientId": client_id,
-                        "token" : token
-                        }
-            return make_response(jsonify(response), 200)
-    else:
-        return "Something went wrong, please try again"
-
 @app.post('/api/client')
 def client_register():
     email = request.json.get("email")
@@ -57,10 +14,10 @@ def client_register():
     lastName = request.json.get("last_name")
     username = request.json.get("username")
     token = uuid.uuid4().hex
-    # pictureUrl = request.json.get("pictureUrl")
+    pictureUrl = request.json.get("pictureUrl")
     keys = ["clientId", "token"]
     response = []
-    results = run_statement("CALL new_client(?, ?, ?, ?, ?, ?)", [email, password, firstName, lastName, username, token])
+    results = run_statement("CALL new_client(?, ?, ?, ?, ?, ?, ?)", [email, password, firstName, lastName, username, token, pictureUrl])
     # results = run_statement("CALL new_client(?, ?, ?, ?, ?, ?, ?)", [token, email, username, firstName, lastName, password, pictureUrl])
     if (type(results) == list):
         for data in results:
@@ -70,3 +27,56 @@ def client_register():
         return "This email is already registered, please login or register with another email."
     else:
         return "Something went wrong, please try again"
+
+@app.post('/api/client-login')
+def login_client():
+    email = request.json.get("email")
+    #get procedure that grabs the hashed password and the id if it exists
+    result = run_statement("CALL get_id(?)", [email])
+    if (result == []):
+        return "Please enter a valid email or register as a new client"
+    if (type(result) == list):
+        password = result[0][1]
+        client_id = result[0][0]
+    pw1 = password.encode('utf-8')
+    #then do pw verification if, else
+    password2 = request.json.get("password")
+    if (bcrypt.checkpw(password2.encode(), pw1)):
+        token = uuid.uuid4().hex
+        results = run_statement("CALL client_login(?, ?)", [token, client_id])
+    # check the row count here; 0 is not successful
+        if results[0][0] == 1:            
+            response = {
+                        "clientId": client_id,
+                        "token" : token
+                        }
+            return make_response(jsonify(response), 200)
+    else:
+        return "Please provide a valid password"
+
+@app.get('/api/client')
+def get_client():
+    token = request.args.get("token")
+    response = []
+    keys = ["clientId", "createdAt", "email", "firstName", "lastName", "pictureUrl", "username"]
+    result = run_statement("CALL get_client(?)", [token])
+    if (type(result) == list):
+        for client in result:
+            print(response)
+            response.append(dict(zip(keys, client)))
+        return make_response(jsonify(response), 200)
+    else:
+        print(result)
+        return make_response(jsonify(result), 500)
+
+@app.patch('/api/client')
+def update_client():
+    token = request.args.get("token")
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    pw = request.json.get("password")
+    salt = bcrypt.gensalt()
+    password = bcrypt.hashpw(pw.encode(), salt)
+    picture_url = request.json.get("pictureUrl")
+    result = run_statement("CALL update_client(?, ?, ?, ?, ?)", [token, first_name, last_name, password, picture_url])
+    print(result)
