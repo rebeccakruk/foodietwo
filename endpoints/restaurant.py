18a65cd170cab55b6e6a8c9c353fc1df5a38d2ef
rk <rebeccakruk@outlook.com>
2023-02-25 16:50:32 -0700

    finished api/restaurant

--- endpoints/restaurant.py
+++ endpoints/restaurant.py
@@ -54,3 +54,30 @@ def get_restaurant():
         return make_response(jsonify(response), 200)
     else:
         return make_response(jsonify(result), 500)
+    
+@app.patch('/api/restaurant')
+def update_restaurant():
+    token = request.args.get("token")
+    result = run_statement("CALL get_resto_id_with_token(?)", [token])
+    if token == None:
+        return "You are not logged in. Please login to update your restaurant information."
+    if (type(result) == list):
+        restaurant_id = result[0][0]
+    name = request.json.get("name")
+    address = request.json.get("address")
+    banner_url = request.json.get("bannerUrl")
+    bio = request.json.get("bio")
+    city = request.json.get("city")
+    phone = request.json.get("phoneNum")
+    profile_url = request.json.get("profileUrl")
+    result = run_statement("CALL update_resto(?, ?, ?, ?, ?, ?, ?, ?)", [restaurant_id, name, address, banner_url, bio, city, phone, profile_url])
+    if result == None:
+        return "You have successfully updated your restaurant profile"
+    elif "Data too long for column 'username_input'" in result:
+        return "Your username is too long. Please choose another username. (maximum 100 characters)"
+    elif "Data too long for column 'first_name_input' at row 0" in result:
+        return "Your first name is too long, please check your entry and try again. (maximum 50 characters)"
+    elif "Data too long for column 'last_name_input' at row 0" in result:
+        return "Your last name is too long, please check your entry and try again. (maximum 50 characters)"
+    else:
+        return "Please try again"
