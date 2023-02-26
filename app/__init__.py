fadf2791bd6b220324fee1d84cd10a9c4c406c76
rk <rebeccakruk@outlook.com>
2023-02-25 18:22:13 -0700

    added menu to init file

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from endpoints import client, client_login, restaurant, restaurant_login
+from endpoints import client, client_login, restaurant, restaurant_login, menu
