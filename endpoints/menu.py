d0580cb831f92d190e0c8763c45656b99696c31c
rk <rebeccakruk@outlook.com>
2023-02-27 19:14:25 -0700

    added orders endpoint

--- endpoints/menu.py
+++ endpoints/menu.py
@@ -1,8 +1,6 @@
 from app import app, __init__
 from flask import jsonify, make_response, request
 from dbhelpers import run_statement
-import bcrypt
-import uuid
 
 @app.post('/api/menu')
 def add_to_menu():
